dca1b955447f138fb12b775bd40a654f9554b330
vuongthanhtuyen <112794129+vuongthanhtuyen@users.noreply.github.com>
2023-12-31 10:11:56 +0700

    fit nullable database

--- Models/AppUser.cs
+++ Models/AppUser.cs
@@ -6,7 +6,7 @@ namespace VTT.models{
     public class AppUser : IdentityUser{
         [Column(TypeName ="nvarchar")]
         [StringLength(400)]
-        public string HomeAdress {get; set;}
+        public string? HomeAdress {get; set;}
 
         [DataType(DataType.Date)]
         public DateTime? BirthDate {get; set;}
